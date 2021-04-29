88c7e18ca373de5985818e6e577ae2334d791aaa
unknown <sergej.jerman1@gmail.com>
2021-04-29 14:02:10 +0200

    prvi commit

---
+++ movies.php
@@ -0,0 +1,23 @@
+<?php
+    include_once "header.php";
+?>
+    <h1>Dobrodošli na strani MOVIES</h1>
+    <?php
+        $b = 110;
+
+        if (isset($_GET['vrednost'])) {
+            $a = $_GET['vrednost'];
+        }
+        else {
+            $a=0;
+        }
+
+        echo $a;
+
+        $a++; // $a = $a+1;
+
+        echo '<a href="index.php?id='.$a.'">INDEX</a>';
+    ?>
+<?php
+    include_once "footer.php";
+?>
